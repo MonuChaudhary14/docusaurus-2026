be3523aded8e19ff87525404d9b4b5008c9677d3
Mayank-Creater <programmayank@gmail.com>
2026-09-02 00:20:46 +0530

    Task 4

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -96,6 +96,11 @@ const config: Config = {
             to: '/docs/monu',
             position: 'left'
           },
+          {
+            label: 'Mayank',
+            to: '/docs/mayank',
+            position: 'left'
+          },
       ],
     },
     // footer: {
