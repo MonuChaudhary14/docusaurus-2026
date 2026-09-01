3251c6f3aeabc8a0a7885596084e0352885b2332
Nandini Chandra <nandinichandra@Nandinis-MacBook-Air.local>
2026-09-02 01:06:09 +0530

    Add Nandini to navbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,6 +79,11 @@ const config: Config = {
         src: 'img/logo.svg',
       },
       items: [
+{
+  label: 'Nandini',
+  to: '/docs/nandini',
+  position: 'left',
+},
         // {
         //type: 'docSidebar',
         //sidebarId: 'tutorialSidebar',
